f0c1ea7d6b9cd9568202af6b8bbd885efd942784
Aires <joaoaires98@gmail.com>
2021-12-03 15:49:09 +0000

    Task 2 done

--- src/main/java/StringTransformerGroup.java
+++ src/main/java/StringTransformerGroup.java
@@ -1,2 +1,15 @@
-public class StringTransformerGroup {
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer{
+    private List<StringTransformer> list;
+
+    public StringTransformerGroup(List<StringTransformer> transformers){
+        this.list = transformers;
+    }
+
+    public void execute(StringDrink drink) {
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).execute(drink);
+        }
+    }
 }
--- src/test/java/StringTransformerGroupTest.java
+++ src/test/java/StringTransformerGroupTest.java
@@ -1,3 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringTransformerGroupTest {
     @Test
     public void transformerGroup() {
@@ -7,9 +14,30 @@ public class StringTransformerGroupTest {
         List<StringTransformer> transformers = new ArrayList<>();
         transformers.add(si);
         transformers.add(cc);
-        StringTransformerGroup tg = new
-                StringTransformerGroup(transformers);
+        StringTransformerGroup tg = new StringTransformerGroup(transformers);
         tg.execute(drink);
         assertEquals("dCbA-DcBa", drink.getText());
+    }
+
+    @Test
+    public void transformerComposite() {
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringInverter si = new StringInverter();
+        StringCaseChanger cc = new StringCaseChanger();
+        StringReplacer sr = new StringReplacer('A', 'X');
+        List<StringTransformer> transformers1 = new ArrayList<>();
+        transformers1.add(si);
+        transformers1.add(cc);
+        StringTransformerGroup tg1 = new StringTransformerGroup(transformers1);
+        List<StringTransformer> transformers2 = new ArrayList<>();
+        transformers2.add(sr);
+        transformers2.add(cc);
+        StringTransformerGroup tg2 = new StringTransformerGroup(transformers2);
+        List<StringTransformer> transformers3 = new ArrayList<>();
+        transformers3.add(tg1);
+        transformers3.add(tg2);
+        StringRecipe recipe = new StringRecipe(transformers3);
+        recipe.mix(drink);
+        assertEquals("DcBx-dCbA", drink.getText());
     }
 }
